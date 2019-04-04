c423787b3ef41d7fbd33962a903cd74503ec4942
admin <admin@Alumno12>
2019-04-04 17:35:07 +0200

    weba

--- Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
+++ Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
@@ -2,8 +2,11 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
 public class Controlador {
 
 	@RequestMapping("/index") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
