ecf4b101fa1f5e7885ea558cc378beb067b20c4b
admin <admin@Alumno12>
2019-04-04 17:31:38 +0200

    cuidado subo el controlador

--- Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
+++ Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class Controlador {
 
+	@RequestMapping("/index") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String inicio(HttpServletRequest request) {
+		return "index";
+	}
 }
