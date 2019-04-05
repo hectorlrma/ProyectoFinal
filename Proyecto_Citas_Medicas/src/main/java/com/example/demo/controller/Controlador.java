5b5c4b8281d4fdd3ad60f88888e87d0ed4a5aae5
admin <admin@Alumno12>
2019-04-05 11:13:31 +0200

    controlador hector prueba

--- Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
+++ Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
@@ -2,15 +2,51 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.beans.Paciente;
+import com.example.demo.interfaces.IPacienteSERVICE;
 
 @Controller
 public class Controlador {
 
+	@Autowired
+	IPacienteSERVICE pacienteSERVICE;
+
 	@RequestMapping("/index") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
-	public String inicio(HttpServletRequest request) {
+	public String index(HttpServletRequest request) {
+		System.out.println("TRAZA INDEX");
 		return "index";
 	}
+
+	@RequestMapping("/registrarse") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String registrarse(HttpServletRequest request) {
+		System.out.println("TRAZA REGISTRARSE");
+		return "registrarse";
+	}
+
+	/*@RequestMapping("/registro") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String registro(HttpServletRequest request, @ModelAttribute Paciente paciente) {
+		System.out.println("TRAZA REGISTRO");
+		Paciente paciente = new Paciente();
+		if (pacienteSERVICE.buscar_idusuario(paciente.getNick_paciente()) == null) {
+			pacienteSERVICE.altapaciente(paciente);
+			request.setAttribute("mensaje", "alta realizada correctamente");
+			return "index";
+		} else {
+			request.setAttribute("mensaje", "usuario ya existe");
+			return "registrarse";
+		}
+	}
+	
+	@RequestMapping("/login") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String registrarse(HttpServletRequest request) {
+		System.out.println("TRAZA LOGIN");
+		return "medico";
+		return "paciente";
+	}*/
+
 }
