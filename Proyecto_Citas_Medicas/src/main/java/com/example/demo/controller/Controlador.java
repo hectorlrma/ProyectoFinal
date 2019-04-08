9e480d77bd954b62cd165e8d03522dd8fd6edb3d
admin <admin@Alumno12>
2019-04-08 11:37:50 +0200

    .

--- Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
+++ Proyecto_Citas_Medicas/src/main/java/com/example/demo/controller/Controlador.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import java.text.ParseException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +12,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.beans.Cita;
+import com.example.demo.beans.Especialidad;
 import com.example.demo.beans.Medico;
 import com.example.demo.beans.Paciente;
+import com.example.demo.dto.CitaDTO;
+import com.example.demo.dto.EspecialidadDTO;
+import com.example.demo.dto.LocalidadDTO;
+import com.example.demo.dto.MedicoDTO;
+import com.example.demo.dto.PacienteDTO;
 import com.example.demo.interfaces.ICitaSERVICE;
+import com.example.demo.interfaces.IEspecialidadSERVICE;
+import com.example.demo.interfaces.ILocalidadSERVICE;
 import com.example.demo.interfaces.IMedicoSERVICE;
 import com.example.demo.interfaces.IPacienteSERVICE;
 
@@ -30,66 +39,82 @@ public class Controlador {
 	ICitaSERVICE citaSERVICE;
 	
 	@Autowired
-	Paciente paciente = new Paciente();
+	IEspecialidadSERVICE especialidadSERVICE;
 	
 	@Autowired
-	Medico medico = new Medico();
+	ILocalidadSERVICE localidadSERVICE;
 	
-	@Autowired
-	Cita cita = new Cita();
+//	Paciente paciente = new Paciente();
+	
+	
+//	Medico medico = new Medico();
+	
+	
+//	Cita cita = new Cita();
 	
 	@RequestMapping("/index") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
 	public String index(HttpServletRequest request) {
 		System.out.println("TRAZA INDEX");
+		HttpSession session = request.getSession(true); // abro sesion
+		List<LocalidadDTO> localidades = localidadSERVICE.listarLocalidades();
+		List<EspecialidadDTO> especialidades=especialidadSERVICE.listarEspecialidades();
+		session.setAttribute("localidades", localidades);
+		session.setAttribute("especialidades", especialidades);
 		return "index";
 	}
 
-	@RequestMapping("/registrarse") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+/*	@RequestMapping("/registrarse") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
 	public String registrarse(HttpServletRequest request) {
 		System.out.println("TRAZA REGISTRARSE");
 		return "registrarse";
-	}
+	}*/
 
-	@RequestMapping("/registro") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
-	public String registro(HttpServletRequest request, @ModelAttribute Paciente paciente) {
-		System.out.println("TRAZA REGISTRO");		
+	@RequestMapping("/registroPaciente") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String registroPaciente(HttpServletRequest request, @ModelAttribute PacienteDTO pacienteDTO) {
+		System.out.println("TRAZA REGISTRO PACIENTE");		
 		HttpSession session = request.getSession(true); // abro sesion
-		if (medico.getEspecialidad() == null) {
-			if (pacienteSERVICE.buscarPaciente(paciente.getNick_paciente()) == null) {
-				pacienteSERVICE.addPaciente(paciente);
+		if (pacienteSERVICE.buscarPacienteDTO(pacienteDTO.getNick_paciente()) == null) {
+				pacienteSERVICE.addPacienteDTO(pacienteDTO);
 				request.setAttribute("mensaje", "alta realizada correctamente");
-				session.setAttribute("nick_paciente", paciente);
+//				session.setAttribute("nick_paciente", paciente);
 				return "index";
 			} else {
 				request.setAttribute("mensaje", "usuario ya existe");
 				return "registrarse";
 			}
-		} else {
-			if (medicoSERVICE.buscarMedico(medico.getNick_medico()) == null) {
-				medicoSERVICE.addMedico(medico);
+		} 
+	
+	@RequestMapping("/registroMedico") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
+	public String registroMedico(HttpServletRequest request, @ModelAttribute MedicoDTO medicoDTO) {
+		System.out.println("TRAZA REGISTRO MEDICO");		
+		HttpSession session = request.getSession(true); // abro sesion
+			if (medicoSERVICE.buscarMedicoDTO(medicoDTO.getNick_medico()) == null) {
+				medicoSERVICE.addMedicoDTO(medicoDTO);
 				request.setAttribute("mensaje", "alta realizada correctamente");
-				session.setAttribute("nick_medico", medico);
+//				session.setAttribute("nick_medico", medico);
 				return "index";
 			} else {
 				request.setAttribute("mensaje", "usuario ya existe");
 				return "registrarse";
 			}
 		}
-	}
+	
 
 	@RequestMapping("/loginpaciente") // ("/")esto quiere decir mi pagina de inicio mapeo a nivel de metodo
 	public String loginpaciente(HttpServletRequest request) {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA LOGIN PACIENTE");
-		session.getAttribute("nick_paciente");
-		paciente=pacienteSERVICE.buscarPaciente(paciente.getNick_paciente());
-		List<Cita> citas = paciente.getCitas();
+//		session.getAttribute("nick_paciente");
+		String nick_paciente = (String)session.getAttribute("nick_paciente");
+		PacienteDTO pacienteDTO=pacienteSERVICE.buscarPacienteDTO(nick_paciente);
+		List<Cita> citas = pacienteDTO.getCitas();
 		List<Cita> citadia = (List<Cita>) new Cita();
 		for (Cita cita : citas) {
 //			if (cita.getFecha_cita()==fechadia()) {
 				citadia.add(cita);
 //			}
 		}
+		
 		request.setAttribute("cita_paciente", citadia);
 		//		return "medico";
 		return "citaPaciente";
@@ -98,14 +123,20 @@ public class Controlador {
 	public String loginmedico(HttpServletRequest request) {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA LOGIN MEDICO");
-		session.getAttribute("nick_medico");
-		medico=medicoSERVICE.buscarMedico(medico.getNick_medico());
-		List<Cita> citas = medico.getCitas();
+//		session.getAttribute("nick_medico");
+		String nick_medico =(String) session.getAttribute("nick_medico");
+		MedicoDTO medicoDTO=medicoSERVICE.buscarMedicoDTO(nick_medico);
+		List<Cita> citas = medicoDTO.getCitas();
 		List<Cita> citadia = (List<Cita>) new Cita();
 		for (Cita cita : citas) {
-//			if (cita.getFecha_cita().compareTo(medicoSERVICE.getFechaDia())==0) {
-				citadia.add(cita);
-//			}
+			try {
+				if (cita.getFecha_cita().compareTo(citaSERVICE.getFechaDia())==0) {
+					citadia.add(cita);
+				}
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		request.setAttribute("cita_medico", citadia);
@@ -117,7 +148,8 @@ public class Controlador {
 	public String borrarcita(HttpServletRequest request) {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA BORRAR CITA");
-		citaSERVICE.borrarCita(cita.getId_cita());
+		int id_cita = (int) request.getAttribute("id_cita");
+		citaSERVICE.borrarCita(id_cita);
 		return "borrarcita";
 	}
 	
@@ -126,7 +158,8 @@ public class Controlador {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA CITA ANTIGUA PACIENTE");
 		session.getAttribute("nick_paciente");
-		paciente=pacienteSERVICE.buscarPaciente(paciente.getNick_paciente());
+		String nick_paciente = (String)session.getAttribute("nick_paciente");
+		PacienteDTO paciente=pacienteSERVICE.buscarPacienteDTO(nick_paciente);
 		List<Cita> citas = paciente.getCitas();
 		List<Cita> citadia = (List<Cita>) new Cita();
 		for (Cita cita : citas) {
@@ -143,7 +176,8 @@ public class Controlador {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA CITA ANTIGUA PACIENTE");
 		session.getAttribute("nick_paciente");
-		paciente=pacienteSERVICE.buscarPaciente(paciente.getNick_paciente());
+		String nick_paciente = (String)session.getAttribute("nick_paciente");
+		PacienteDTO paciente=pacienteSERVICE.buscarPacienteDTO(nick_paciente);
 		List<Cita> citas = paciente.getCitas();
 		List<Cita> citadia = (List<Cita>) new Cita();
 		for (Cita cita : citas) {
@@ -159,9 +193,9 @@ public class Controlador {
 	public String citaantiguamed(HttpServletRequest request) {
 		HttpSession session = request.getSession(true); // abro sesion
 		System.out.println("TRAZA CITA ANTIGUA MEDICO");
-		session.getAttribute("nick_medico");
-		medico=medicoSERVICE.buscarMedico(medico.getNick_medico());
-		List<Cita> citas = medico.getCitas();
+		String nick_medico = (String) session.getAttribute("nick_medico");
+		MedicoDTO medicoDTO=medicoSERVICE.buscarMedicoDTO(nick_medico);
+		List<Cita> citas = medicoDTO.getCitas();
 		List<Cita> citadia = (List<Cita>) new Cita();
 		for (Cita cita : citas) {
 //			if (cita.getFecha_cita().compareTo(medicoSERVICE.getFechaDia())<0) {
@@ -177,9 +211,9 @@ public class Controlador {
 	public String citaproximamed(HttpServletRequest request) {
 	HttpSession session = request.getSession(true); // abro sesion
 	System.out.println("TRAZA CITA PROXIMA MEDICO");
-	session.getAttribute("nick_medico");
-	medico=medicoSERVICE.buscarMedico(medico.getNick_medico());
-	List<Cita> citas = medico.getCitas();
+	String nick_medico = (String) session.getAttribute("nick_medico");
+	MedicoDTO medicoDTO=medicoSERVICE.buscarMedicoDTO(nick_medico);
+	List<Cita> citas = medicoDTO.getCitas();
 	List<Cita> citadia = (List<Cita>) new Cita();
 	for (Cita cita : citas) {
 //		if (cita.getFecha_cita().compareTo(medicoSERVICE.getFechaDia())>0) {
